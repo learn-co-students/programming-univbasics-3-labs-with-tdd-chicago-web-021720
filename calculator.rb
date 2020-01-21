c8f6e1d1fd2c229d31c479b6f7a5f0be972a378a
bryan lesht <bryan.lesht@colorado.edu>
2020-01-21 18:04:05 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,20 @@
 # Add your variables here
+first_number = 5
+second_number = 2 
+def sumfunc(first,second)
+  first+second
+end
+sum = sumfunc(first_number,second_number)
+def difFunc(first,second)
+  first-second
+end
+difference = difFunc(first_number,second_number)
+
+def prodFunc(first,second)
+  first*second
+end
+product=prodFunc(first_number,second_number)
+def quotFunc(first,second)
+  first/second
+end
+quotient = quotFunc(first_number,second_number)
